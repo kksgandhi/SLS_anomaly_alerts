2dec6c576f813fbc09693643cef907b8f592fcd0
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-12 16:19:58 -0400

    send email with interesting sensor pictures

--- controller.py
+++ controller.py
@@ -1,8 +1,10 @@
 import analysis
 import emailer
 import config as conf
+import os
 
 def daily_everything():
+    list(map(os.remove, emailer.get_all_pngs_in_current_dir()))
     daily_data = analysis.daily_test()
     daily_data.to_csv(conf.CSV_OUTFILE)
     emailer.daily_mail()
--- emailer.py
+++ emailer.py
@@ -11,14 +11,14 @@ from yagmail import inline
 from datetime import date
 import os
 
-def send_daily():
+def daily_mail():
     with yagmail.SMTP(p_conf.USERNAME, p_conf.PASSWORD) as sender:
-        params                = {}
-        params["to"]          = p_conf.TO_LIST
-        params["subject"]     = f"{date.today()} anomaly updates"
-        params["contents"]    = ["General Kenobi",
-                                 inline('./lava.jpg')]
-        params["attachments"] = ['./test.csv']
+        params                 = {}
+        params["to"]           = p_conf.TO_LIST
+        params["subject"]      = f"{date.today()} anomaly updates"
+        params["contents"]     = ["Attached are interesting sensors"]
+        params["attachments"]  = [conf.CSV_OUTFILE]
+        params["attachments"] += get_all_pngs_in_current_dir()
         sender.send(**params)
 
 def get_all_pngs_in_current_dir():
