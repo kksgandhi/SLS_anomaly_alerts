dff95e4cae6286f3d8b6e10e0c8615a301ebd7f7
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-12 15:31:16 -0400

    exit when no private config found

--- emailer.py
+++ emailer.py
@@ -4,6 +4,8 @@ except ModuleNotFoundError:
     print("No private_config found!")
     print("Fill out private_config_example.py and copy it to private_config.py")
     print("DO NOT ADD YOUR PRIVATE_CONFIG TO VERSION CONTROL")
+    import sys
+    sys.exit()
 import config as conf
 import yagmail
 from yagmail import inline
