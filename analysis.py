55fa811a87095cea672e720f66a5c9ab3cf9f393
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-15 17:52:45 -0400

    flag calculation moved into full test

--- analysis.py
+++ analysis.py
@@ -249,22 +249,18 @@ def full_sensor_test(sensor, **kwargs):
         params = fit_curve(train, ftp_function, **kwargs)
         train_residuals = calculate_residuals(train, params, ftp_function, **kwargs)    
         test_res_all = list(map(lambda curr_test: calculate_residuals(curr_test, params, ftp_function, **kwargs), test_all))
-        if save_plots:
-            sensor_id = sensor["name"]
-            to_plot = False
-            to_plot = to_plot or (test_res_all[0] > conf.ONE_HOUR_THRESHOLD
-                                  and sensor_id not in conf.ONE_HOUR_IGNORE)
-            to_plot = to_plot or (test_res_all[1] > conf.ONE_DAY_THRESHOLD
-                                  and sensor_id not in conf.ONE_DAY_IGNORE)
-            to_plot = to_plot or (test_res_all[2] > conf.THREE_DAYS_THRESHOLD
-                                  and sensor_id not in conf.THREE_DAYS_IGNORE)
-            num_pts_day = 0 if not num_pts_day else num_pts_day
-            to_plot = to_plot or (num_pts_day < conf.MIN_VALUES_PER_DAY
-                                  and sensor_id not in conf.MIN_VALUES_IGNORE)
-            if to_plot:
-                plot(test_all[2], params, ftp_function, plot_name=sensor_name,**kwargs)
+
+        sensor_id = sensor["name"]
+        flag_1hour    = test_res_all[0] > conf.ONE_HOUR_THRESHOLD and sensor_id not in conf.ONE_HOUR_IGNORE
+        flag_1day     = test_res_all[1] > conf.ONE_DAY_THRESHOLD and sensor_id not in conf.ONE_DAY_IGNORE
+        flag_3days    = test_res_all[2] > conf.THREE_DAYS_THRESHOLD and sensor_id not in conf.THREE_DAYS_IGNORE
+        flag_min_vals = num_pts_day < conf.MIN_VALUES_PER_DAY and sensor_id not in conf.MIN_VALUES_IGNORE
+        flags = (flag_1hour, flag_1day, flag_3days, flag_min_vals)
+
+        if save_plots and (flag_1hour or flag_1day or flag_3days or flag_min_vals):
+            plot(test_all[2], params, ftp_function, plot_name=sensor_name,**kwargs)
         print("Success: " + sensor_name)
-        return train_residuals, test_res_all, num_pts_day
+        return train_residuals, test_res_all, num_pts_day, flags
     except Exception as e:
         if kwargs.get("verbose"):
             raise e
@@ -280,18 +276,11 @@ def daily_test():
     sensors["test_residuals_1day"]  = residuals.apply(lambda res: res[1][1] if res else None)
     sensors["test_residuals_3days"] = residuals.apply(lambda res: res[1][2] if res else None)
     sensors["num_test_vals"]        = residuals.apply(lambda res: res[2]    if res else 0)
-    sensors["flag_1hour"] = (
-        (sensors["test_residuals_1hour"] > conf.ONE_HOUR_THRESHOLD) &
-        (sensors["name"].apply(lambda x: x not in conf.ONE_HOUR_IGNORE)))
-    sensors["flag_1day"] = (
-        (sensors["test_residuals_1day"] > conf.ONE_DAY_THRESHOLD) &
-        (sensors["name"].apply(lambda x: x not in conf.ONE_DAY_IGNORE)))
-    sensors["flag_3days"] = (
-        (sensors["test_residuals_3days"] > conf.THREE_DAYS_THRESHOLD) &
-        (sensors["name"].apply(lambda x: x not in conf.THREE_DAYS_IGNORE)))
-    sensors["flag_min_vals"] = (
-        (sensors["num_test_vals"] < conf.MIN_VALUES_PER_DAY) &
-        (sensors["name"].apply(lambda x: x not in conf.MIN_VALUES_IGNORE)))
+    sensors["flag_1hour"]           = residuals.apply(lambda res: res[3][0] if res else False)
+    sensors["flag_1day"]            = residuals.apply(lambda res: res[3][1] if res else False)
+    sensors["flag_3days"]           = residuals.apply(lambda res: res[3][2] if res else False)
+    sensors["flag_min_vals"]        = residuals.apply(lambda res: res[3][3] if res else False)
+
     sensors = sensors.sort_values("num_test_vals", ascending=False)  
     sensors["run_date"] = datetime.datetime.utcnow()
     return sensors
