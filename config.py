c380042d0e78c1f35c998dae6c6efc2b39ac8817
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-15 13:36:24 -0400

    formatting

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ THREE_DAYS_THRESHOLD = 300
 MIN_VALUES_PER_DAY   = 200
 # list of sensors to ignore (known interesting sensors)
 # ex: ['gt-evnsense-030', 'gt-envsense-025']
-ONE_HOUR_IGNORE      = []
-ONE_DAY_IGNORE       = []
-THREE_DAY_IGNORE     = []
-MIN_VALUES_IGNORE    = []
+ONE_HOUR_IGNORE   = []
+ONE_DAY_IGNORE    = []
+THREE_DAYS_IGNORE = []
+MIN_VALUES_IGNORE = []
 # name of file where daily output should be stored
 CSV_OUTFILE = './daily_output.csv'
