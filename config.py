9fc013e660981c2289a99ce20c7efca305fbc420
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-19 10:31:12 -0400

    add sensor groupings to config

--- config.py
+++ config.py
@@ -15,3 +15,12 @@ THREE_DAYS_IGNORE = []
 MIN_VALUES_IGNORE = []
 # name of file where daily output should be stored
 CSV_OUTFILE = './daily_output.csv'
+SENSOR_GROUPINGS = {
+	"group1": [ "Hwy 21 at St Augustine Creek Level Sensor", "Houlihan Boat Ramp Sea Level Sensor", "Dean Forest Road at Harden Canal Sea Level Sensor", "Hwy 17 at Salt Creek Sea Level Sensor" ],
+	"group2": [ "Fort Pulaski (A) Sea Level Sensor", "Fort Pulaski (B) Sea Level Sensor", "Catalina Drive Sea Level Sensor", "Lazaretto Creek Fishing Pier Sea Level Sensor", "Hwy 80 at Chimney Creek on Tybee Island Sea Level Sensor" ],
+	"group3": ["Diamond Causeway at Shipyard Creek Sea Level Sensor", "Shipyard Road Sea Level Sensor", "Sullivan Street Sea Level Sensor", "Hunt Drive on Burnside Island Sea Level Sensor", "Faye Drive on Burnside Island Sea Level Sensor" ],
+	"group4": [ "Coffee Bluff Marina environmental sensors", "Rose Dhu Island Sea Level Sensor", "Kilkenny Creek environmental sensors", "Sapelo Island Ferry Dock Sea Level Sensor" ],
+	"group5": [ "Skidaway Road at Herb River Sea Level Sensor", "Savannah State Sea Level Sensor", "LaRoche Avenue at Nottingham Creek Sea Level Sensor", "Solomon Bridge Sea Level Sensor"],
+	"group6": [ "Oatland Island Road environmental sensors", "Hwy 80 at Grays Creek Sea Level Sensor"],
+	"group7": ["Landings Harbor Marina Sea Level Sensor","SKIO Dock Sea Level Sensor", "Turner Creek Boat Ramp Sea Level Sensor", "Walthour Road environmental sensors", "Bull River Marina environmental sensors" ]
+}
